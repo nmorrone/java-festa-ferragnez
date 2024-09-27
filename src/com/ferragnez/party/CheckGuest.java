1aab1965326428ae437ce3aae7a8103fbd1a3c67
nmorrone <n.morrone96@gmail.com>
2024-09-27 20:18:13 +0200

    invited-people-array

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -4,6 +4,15 @@ public class CheckGuest {
 	
 	public static void main(String[]args) {
 		
+		/* l'utente inserirà il suo nome per vedere se è presente nella
+		 * lista invitati della festa, se il nome è presente gli sarà consentito
+		 * l'accesso altrimenti gli verrà negato */
+
+		//creo array con lista invitati
+		
+		String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", 
+							"Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
+		
 		
 	}
 
