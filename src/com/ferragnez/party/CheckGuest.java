3f12d01de168977ea044a7fc9d9d43c83eeff96d
nmorrone <n.morrone96@gmail.com>
2024-09-28 14:59:24 +0200

    String-for-cicle-open

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,5 +1,7 @@
 package com.ferragnez.party;
 
+import java.util.Scanner;
+
 public class CheckGuest {
 	
 	public static void main(String[]args) {
@@ -13,6 +15,29 @@ public class CheckGuest {
 		String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", 
 							"Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
 		
+		//verifico funzionamento
+		System.out.println("Benvenuto nel CheckGuest della Festa");
+		System.out.println("Controlliamo subito se sei fai parte degli invitati");
+		
+		
+		//chiediamo all'utente di inserire il nome
+		Scanner userName = new Scanner(System.in);
+		System.out.println("Inserisci qui sotto il tuo Nome e Cognome");
+		//salviamo il valore in una variabile
+		String user = userName.next();
+		//verifico acquisizione
+		//System.out.println(user);
+	
+		
+		for (int i = 0; i < invitati.length; i++) {
+			
+			//definisco la variabile che cambiera di volta in volta per il controlo
+			//acquisira ogni valore singolo dell'Array ad ogni ciclo
+			String nameCheck = invitati[i];
+			//controllo funzionamento ciclo for
+			System.out.println(nameCheck);
+			
+		}
 		
 	}
 
