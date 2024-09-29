2c774d809c593e7a9ac45981e5eb2d9c6772a82d
nmorrone <n.morrone96@gmail.com>
2024-09-29 17:03:08 +0200

    comparison-message.success

--- src/com/ferragnez/party/CheckGuest.java
+++ src/com/ferragnez/party/CheckGuest.java
@@ -1,44 +1,40 @@
 package com.ferragnez.party;
-
 import java.util.Scanner;
-
 public class CheckGuest {
+
 	
 	public static void main(String[]args) {
-		
 		/* l'utente inserirà il suo nome per vedere se è presente nella
 		 * lista invitati della festa, se il nome è presente gli sarà consentito
 		 * l'accesso altrimenti gli verrà negato */
 
 		//creo array con lista invitati
+		String [] invitati = {"Dua Lipa","Paris Hilton","Manuel Agnelli","J-Ax","Francesco Totti","Ilary Blasi","Bebe Vio","Luis","Pardis Zarei","Martina Maccherone","Rachel Zeilic"};
+		//apro il metodo scanner
+		Scanner keyboardInput = new Scanner(System.in);
 		
-		String[] invitati = {"Dua Lipa", "Paris Hilton", "Manuel Agnelli", "J-Ax", "Francesco Totti", 
-							"Ilary Blasi", "Bebe Vio", "Luis", "Pardis Zarei", "Martina Maccherone", "Rachel Zeilic"};
-		
-		//verifico funzionamento
-		System.out.println("Benvenuto nel CheckGuest della Festa");
-		System.out.println("Controlliamo subito se sei fai parte degli invitati");
-		
-		
-		//chiediamo all'utente di inserire il nome
-		Scanner userName = new Scanner(System.in);
-		System.out.println("Inserisci qui sotto il tuo Nome e Cognome");
-		//salviamo il valore in una variabile
-		String user = userName.next();
-		//verifico acquisizione
+		//chiedo all'utente il nome e salvo il valore
+		System.out.println("Benvenuto nel CheckGuest della Festa, inserisci il tuo nome qui sotto:");
+		String user = keyboardInput.nextLine();
+		//controllo
 		//System.out.println(user);
-	
 		
-		for (int i = 0; i < invitati.length; i++) {
+		//definisco var che mi servirà dopo il controllo per il messaggio in uscita
+		int condition = 0;
+		
+		//apro ciclo for per confrontare il nome inserito con quelli presente in lista
+		for(int i = 0; i < invitati.length; i++) {
+			//apro condizione di uguaglianza
+			if(user.equals(invitati[i])) {
+				condition = 1;
+				System.out.println(condition);
+			}
 			
-			//definisco la variabile che cambiera di volta in volta per il controlo
-			//acquisira ogni valore singolo dell'Array ad ogni ciclo
-			String nameCheck = invitati[i];
-			//controllo funzionamento ciclo for
-			System.out.println(nameCheck);
-			
+			//apro condizione per il messaggio
+			if(condition == 1) {
+				
+				System.out.println("Complimenti " + user + " goditi la Festa!");
+			}
 		}
-		
 	}
-
 }
